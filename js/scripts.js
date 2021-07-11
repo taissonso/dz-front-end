058c6c1f7815b6f569cd97a8ca1e23f994a73c82
taisson <taisson.oliveira@gmail.com>
2021-07-11 16:17:16 -0300

    Update project responsive

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,12 @@ onload = () => {
         qdtErros = qdtErros + validadeEmail();
         qdtErros = qdtErros + validadePhone();
         qdtErros = qdtErros + validadeMensagem();
-        console.log(qdtErros)
         if (qdtErros == 0) {
-           abrirModal();
-           ev.preventDefault(); 
-           document.querySelector('.form').reset();
+            abrirModal();
+            ev.preventDefault();
+            document.querySelector('.form').reset();
         } else {
-            ev.preventDefault(); 
+            ev.preventDefault();
         }
     }
 
@@ -121,9 +120,49 @@ onload = () => {
         document.querySelector('#modal').style.display = 'none';
     }
 
+    var indexGeral = 1;
+    showSlides(indexGeral);
+
+    const updateSlide = (novo) => {
+        showSlides(indexGeral = indexGeral + novo);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("slides");
+        
+        if (n > slides.length) {
+            indexGeral = 1;
+        }
+        if (n < 1) {
+            indexGeral = slides.length;
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        
+        slides[indexGeral - 1].style.display = "flex";
+    }
+
+    const openMenu = () => {
+        let menu = document.querySelector('#nav__menu');
+        
+        menu.className === "nav__bar" ?  menu.className += " nav__open" :  menu.className = "nav__bar";
+    }
+
+    const closeMenu = () => {
+        let menu = document.querySelector('#nav__menu');
+        menu.className === "nav__bar" ?  menu.className -= " nav__open" :  menu.className = "nav__bar";
+    }
+
+
     document.querySelector('#btn__submit').onclick = validateForm;
     document.querySelector('#btn__modal').onclick = fecharModal;
     document.querySelector('#modal').onclick = fecharModal;
-
     document.querySelector('#phone').onkeyup = maskPhone;
+    document.querySelector('#menu__icon').onclick = openMenu;
+    document.querySelector('#close__icon').onclick = closeMenu;
+    document.querySelector('.next').onclick = () => updateSlide(1);
+    document.querySelector('.prev').onclick = () => updateSlide(-1);
+   
 }
